bc5f2456c07625ba0ed62b5ad33303c142657fd7
OliLabSLMS-2025 <olilab.slms.2025@gmail.com>
2025-09-18 10:25:32 +0800

    Delete OliLab Science Laboratory Management System v.09.17.11 directory

--- OliLab Science Laboratory Management System v.09.17.11/constants.ts
+++
@@ -1,8 +0,0 @@
-// Grade levels and their corresponding sections
-export const GRADE_LEVELS = {
-    'Grade 11': ['Altruism', 'Benevolence', 'Creativity'],
-    'Grade 12': ['Diplomacy', 'Efficiency', 'Fairness'],
-};
-
-// Standardized item categories
-export const ITEM_CATEGORIES = ['Physics', 'Biology', 'Chemistry', 'Mathematics', 'Others'];
